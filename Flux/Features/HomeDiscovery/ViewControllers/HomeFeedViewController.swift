1a4c0a895817968c102504ff43fa9bdc9560aebf
aalsaffar <alialsaffarbusiness@gmail.com>
2025-12-15 15:11:52 +0300

    Bind LoginVC to AuthViewModel and add navigation flow.

--- Flux/Features/HomeDiscovery/ViewControllers/HomeFeedViewController.swift
+++ Flux/Features/HomeDiscovery/ViewControllers/HomeFeedViewController.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+
+import UIKit
+
+class HomeFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("✅ HomeFeedViewController Loaded Successfully")
+    }
+}
